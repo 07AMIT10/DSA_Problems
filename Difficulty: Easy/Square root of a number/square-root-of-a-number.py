bc2d8ba10b8bf6bb7093efde26fdee3b564c085e
AMIT T <68996330+07AMIT10@users.noreply.github.com>
2024-08-13 21:53:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square root of a number/square-root-of-a-number.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+
+#Complete this function
+class Solution:
+    def floorSqrt(self, n): 
+    #Your code here
+        sd = n**.5
+        return int(sd)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        x = int(input())
+
+        print(Solution().floorSqrt(x))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
